918ca21b83ee9f694a82d670169a3502eff5c704
kishorchouhan <devkishorbwr@gmail.com>
2019-03-06 11:08:40 +0530

    L5 end. Profanity check project created

---
+++ Lession 5/check_profanity.py
@@ -0,0 +1,23 @@
+import urllib
+
+def read_text():
+    quotes = open("C:\Users\Nanu\Desktop\Programming Foundations with Python ud036\Lession 5\movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
+    quotes.close()
+    check_profanity(contents_of_file)
+
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    #print(output)
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert!!")
+    elif "false" in output:
+        print ("This document has no curse word!")
+    else:
+        print("Could not scan the document properly.")
+    
+read_text()
